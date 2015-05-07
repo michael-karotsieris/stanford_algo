d900020e7abc286017c9832d4c01f5ec00d4e4ef
michael_karotsieris <michael.karotsieris@gmail.com>
2015-05-07 19:44:43 +0100

    added bfs_shortest_path

--- BFS/bfs_shortest_path.py
+++ BFS/bfs_shortest_path.py
@@ -1,20 +1,24 @@
 
 
-def bfs(G , S):# graph = G , start_vertex = s , supose G is a dictionary
+def bfs(G , S , V):# graph = G , start_vertex = s , supose G is a dictionary
 	graph_queue = [S]
 	visited = []
+	dist = {}
+	dist[S] = 0
+	if S != V:
+		dist[V] = float( '+inf' )
 	print "Graph Queue initial"
 	print graph_queue
 	while graph_queue:
 		next_node = graph_queue.pop(0)
 		for edge in G[next_node] :
 			if edge not in visited:
+				dist[edge] = dist[next_node] + 1
 				visited.append(edge)
 				print "Visited Node: " + str(edge)
 				graph_queue.append(edge)
-					
+	return dist[V]				
 					
 					
 
-
-bfs({1:[2,3] , 2:[1,3,4,5] , 3:[1,2,4] , 4:[2,3] , 5:[2]} , 1)
+print bfs({1:[2,3] , 2:[1,3,4,5] , 3:[1,2,4] , 4:[2,3] , 5:[2,6] , 6:[5] , 7:[]} , 4 , 7)
